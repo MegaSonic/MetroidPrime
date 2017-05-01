b1eeef11207c4346e38713f1d9a4434b601648af
Adam Kay <u0988616@ad.utah.edu>
2017-05-01 17:43:55 -0600

    Polishing some things

---
+++ Assets/CameraSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public GameObject cameraToSwitch;
+    public bool canSwitch = true;
+    public GameObject activeCamera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (canSwitch)
+            {
+                if (cameraToSwitch.activeSelf)
+                {
+                    cameraToSwitch.SetActive(false);
+                    activeCamera.SetActive(true);
+                }
+                else
+                {
+                    activeCamera.SetActive(false);
+                    cameraToSwitch.SetActive(true);
+                }
+                canSwitch = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canSwitch = true;
+    }
+}
